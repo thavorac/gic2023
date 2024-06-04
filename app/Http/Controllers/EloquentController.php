5cf732353d2d57dfc9fb5b0e755b9b72ed674680
Thavorac <thavorac@sala.co>
2024-06-04 08:59:27 +0700

    Call eloquent functions

--- app/Http/Controllers/EloquentController.php
+++ app/Http/Controllers/EloquentController.php
@@ -3,16 +3,27 @@
 namespace App\Http\Controllers;
 
 use App\Models\Order;
+use App\Models\Product;
 use App\Models\User;
 use Illuminate\Http\Request;
 
 class EloquentController extends Controller
 {
     public function test(){
-        $userA = User::where('id', 1)->with(['orders'])->get();
+        // TODO: Get all products of the user 1
 
-        //$orders = Order::where('user_id', 1)->get();
-        //$orders = $userA->orders;
-        dd($userA);
+        // Option 1
+        // $user1 = User::with(['orders', 'orders.products'])->find(1);
+
+        // Option 2
+        $user1 = User::find(1);
+        $orders = $user1->orders;
+        $products = [];
+
+        for ($i=0;$i<count($orders); $i++) {
+            $order = $orders[$i];
+            dd($order->products->toArray());
+        }
+
     }
 }
